05bfd41795a3e7e142d259c65bf581325cce7bd7
erhankabuk <erhankabuk@hotmail.com>
2022-01-03 18:23:50 +0300

    UnitTests

---
+++ tests/UnitTests/Applicationcore/Services/BasketServiceTests/DeleteBasket.cs
@@ -0,0 +1,56 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Services;
+using Ardalis.Specification;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Applicationcore.Services.BasketServiceTests
+{
+    public class DeleteBasket
+    {
+        private readonly Mock<IRepository<Basket>> _mockBasketRepository = new();
+        private const string _buyerId = "AnExampleBuyerId";
+        [Fact]
+        public async Task ShouldInvokeBasketRepositoryDeleteAsyncOnce()
+        {
+
+            Basket basket = new Basket()
+            {
+                Id = 1,
+                BuyerId = _buyerId,
+                Items = new List<BasketItem>()
+                {
+
+                    new BasketItem()
+                    {
+                        Id = 1,
+                        BasketId = 1,
+                        ProductId = 3,
+                        Quantity = 4
+                    },
+                    new BasketItem()
+                    {
+                        Id = 2,
+                        BasketId = 2,
+                        ProductId = 4,
+                        Quantity = 2
+                    }
+                }
+
+            };
+            _mockBasketRepository.Setup(x => x.FirstOrDefaultAsync(It.IsAny<Specification<Basket>>())).ReturnsAsync(basket);
+            
+            var basketService = new BasketService(_mockBasketRepository.Object);
+            await basketService.DeleteBasketAsync(_buyerId);
+            _mockBasketRepository.Verify(x => x.DeleteAsync(It.IsAny<Basket>()), Times.Once);
+
+
+        }
+    }
+}
